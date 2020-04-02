4b47c2246dbd21aec30a6a91bb2bf4726a321ed7
username <user@email.mail>
2020-04-02 21:29:35 +0900

    my-app

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [];
+
+import { ShippingComponent } from './shipping/shipping.component';
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
